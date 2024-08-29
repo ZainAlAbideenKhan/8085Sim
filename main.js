44e91a887a6aa6711e1d603a2f8356ae51b0d316
Abdul Zainul Khan <zainulavdeenkhan@outlook.com>
2024-08-29 05:54:44 +0530

    Adjusted Size of MP on Load.

--- main.js
+++ main.js
@@ -8,33 +8,21 @@ function isMobileDevice() {
   );
 }
 
-let mp = new MP8085(["0000", "FFFF"], "16Bits");
-window.MP8085 = MP8085;
-window.mp = mp;
-
 // UI
 document.querySelector(".size-selector").addEventListener("input", (event) => {
   let scale_val = event.target.value / 100;
+  setKitScale(scale_val);
+});
+function setKitScale(scale_val) {
   scale_val = scale_val < 0.4 ? 0.4 : scale_val;
   document.querySelector(".microprocessor8085").style.scale = scale_val;
-});
+}
 
 document.querySelectorAll('input[name="screenColor"]').forEach((i) => {
   i.addEventListener("change", (event) => {
     let col = event.target.value;
     mp.htm_screen.style.backgroundColor = col;
   });
-});
-
-mp.htm_reset_btn.addEventListener("click", function (event) {
-  mp.registers.reset();
-  opp_cache.reset();
-  mp.start();
-  event.target.blur();
-});
-
-mp.htm_power_btn.addEventListener("click", (event) => {
-  mp.power();
 });
 
 if (isMobileDevice()) {
@@ -58,7 +46,30 @@ if (isMobileDevice()) {
   document.querySelector(".enable-keyboard input").remove();
 }
 
-// initialisation
-mp.start();
+window.onload = () => {
+  // adjust microprocessor scale on load
+  if (document.body.clientHeight > window.innerHeight * 0.8) {
+    let scale_val = (window.innerHeight * 0.8) / document.body.clientHeight;
+    setKitScale(scale_val);
+    scale_val *= 100;
+    document.querySelector(".size-selector").value = scale_val;
+  }
 
-export { mp };
+  let mp = new MP8085(["0000", "FFFF"], "16Bits");
+  window.mp = mp;
+
+  // event listners
+  mp.htm_reset_btn.addEventListener("click", function (event) {
+    mp.registers.reset();
+    opp_cache.reset();
+    mp.start();
+    event.target.blur();
+  });
+  
+  mp.htm_power_btn.addEventListener("click", (event) => {
+    mp.power();
+  });
+  
+  // initialisation
+  mp.start();
+};
